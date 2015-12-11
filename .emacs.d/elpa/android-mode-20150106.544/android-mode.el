b3449ada9da20e37181fd3b5e5cec1b6ebb5074d
samgh96 <samgh96@gmail.com>
2015-12-11 19:41:58 +0000

    Actualizado 11-12-2015

---
+++ .emacs.d/elpa/android-mode-20150106.544/android-mode.el
@@ -0,0 +1,73 @@
+<html> 
+<head>
+	<title>Portal de acceso a la red fiwifi</title>
+</head>
+<body> 
+<form method="post" action="https://fiwifi.fi.upm.es:8003/"> 
+	<input name="redirurl" type="hidden" value="http://melpa.org/packages/android-mode-20150106.544.el">
+	<input name="zone" type="hidden" value="fiwifi">
+	<center>
+	<table cellpadding="6" cellspacing="0" width="550" height="380" style="border:1px solid #000000">
+	<tr>
+		<td><img src="/captiveportal-logo_etsiinf.gif"></td>
+	</tr>
+	<tr><td style="text-align:center; font:1.7em Tahoma, Verdana, Arial, Helvetica, sans-serif; background-color:#0099ee; color:white">
+		Bienvenido al portal de acceso a la red <b>fiwifi</b>
+	</td></tr>
+	<tr><td>
+		<div id="mainlevel">
+		<center>
+		<table width="100%" border="0" cellpadding="5" cellspacing="0">
+		<tr><td>
+			<center>
+			<div id="mainarea">
+			<center>
+			<table width="100%" border="0" cellpadding="5" cellspacing="5">
+			<tr><td>
+				<div id="maindivarea">
+				<center>
+					<div id='statusbox'>
+						<font color='red' face='arial' size='+1'><b></b></font>
+					</div>
+					<br/>
+					<div id='loginbox'>
+					<table>
+					<tr><td colspan="3" style="text-align:center; color:#88ac0b; font:bold 125% Tahoma, Verdana, Arial, Helvetica, sans-serif">
+						Introduzca sus datos
+					</td></tr> 
+					<tr><td>&nbsp;</td></tr>
+					<tr>
+						<td colspan="2" style="text-align:right; padding-right:5px; color:#88ac0b; font:bold Tahoma, Verdana, Arial, Helvetica, sans-serif">
+							Usuario
+						</td>
+						<td><input name="auth_user" type="text" style="border: 1px solid;"></td>
+					</tr>
+					<tr>
+						<td colspan="2" style="text-align:right; padding-right:5px; color:#88ac0b; font:bold Tahoma, Verdana, Arial, Helvetica, sans-serif">
+							Clave
+						</td>
+						<td><input name="auth_pass" type="password" style="border:1px solid;"></td>
+					</tr> 
+					<tr><td>&nbsp;</td></tr>
+					<tr>
+						<td colspan="3"><center><input name="accept" type="submit" value="Inicie Sesi&oacute;n"></center></td>
+					</tr>
+					</table>
+					</div>
+				</center>
+				</div>
+			</td></tr>
+			</table>
+			</center>
+			</div>
+			</center>
+		</td></tr>
+		</table>
+		</center>
+		</div>
+	</td></tr>
+	</table>
+	</center>
+</form>
+</body> 
+</html>
---
+++ .emacs.d/elpa/twilight-theme-20120412.603/twilight-theme-autoloads.el
@@ -0,0 +1,16 @@
+;;; twilight-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("twilight-theme.el") (22090 10418 243769
+;;;;;;  112000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; twilight-theme-autoloads.el ends here
